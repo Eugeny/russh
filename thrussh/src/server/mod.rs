90eb52a3c6764371d7b5a91eac775c32a719479d
Eugene Pankov <e@ajenti.org>
2022-02-14 23:17:53 +0100

    added channel_success

--- thrussh/src/server/mod.rs
+++ thrussh/src/server/mod.rs
@@ -548,6 +548,9 @@ where
                     Some((id, ChannelMsg::Close)) => {
                         session.close(id);
                     }
+                    Some((id, ChannelMsg::Success)) => {
+                        session.channel_success(id);
+                    }
                     Some((id, ChannelMsg::XonXoff { client_can_do })) => {
                         session.xon_xoff_request(id, client_can_do);
                     }
@@ -559,9 +562,6 @@ where
                     }
                     Some((id, ChannelMsg::WindowAdjusted { new_size })) => {
                         debug!("window adjusted to {:?} for channel {:?}", new_size, id);
-                    }
-                    Some((id, ChannelMsg::Success)) => {
-                        debug!("channel success {:?}", id);
                     }
                     None => {
                         debug!("session.receiver: received None");
