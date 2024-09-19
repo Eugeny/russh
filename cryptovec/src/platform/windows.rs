ddfa6be9746205af397ea8a3964739c66deed465
irving ou <jou@devolutions.net>
2024-09-19 14:10:22 -0400

    fix test and comments

--- cryptovec/src/platform/windows.rs
+++ cryptovec/src/platform/windows.rs
@@ -21,3 +21,9 @@ pub fn memset(ptr: *mut u8, value: i32, size: usize) {
         libc::memset(ptr as *mut c_void, value, size);
     }
 }
+
+pub fn memcpy(dest: *mut u32, src: *const u8, size: usize) {
+    unsafe {
+        libc::memcpy(dest as *mut c_void, src as *const c_void, size);
+    }
+}
