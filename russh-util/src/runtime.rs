e17ee82e321e08b811609c638b88601486781edc
Eugene <inbox@null.page>
2024-09-20 21:50:35 +0200

    simplify spawn runtime

--- russh-util/src/runtime.rs
+++ russh-util/src/runtime.rs
@@ -1,110 +1,82 @@
-#[cfg(not(target_arch = "wasm32"))]
-pub use native::*;
-#[cfg(target_arch = "wasm32")]
-pub use wasm::*;
+use std::io::ErrorKind;
+use std::ops::Deref;
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
 
 #[derive(Debug)]
-pub struct JoinError {
-    #[cfg(not(target_arch = "wasm32"))]
-    inner: tokio::task::JoinError,
-    #[cfg(target_arch = "wasm32")]
-    inner: tokio::sync::oneshot::error::RecvError,
+pub struct JoinError(Box<dyn std::error::Error + Send + Sync>);
+
+impl Deref for JoinError {
+    type Target = dyn std::error::Error + Send + Sync;
+
+    fn deref(&self) -> &Self::Target {
+        &*self.0
+    }
 }
 
 impl std::fmt::Display for JoinError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "JoinError: {}", self.inner)
+        self.0.fmt(f)
     }
 }
 
-impl std::error::Error for JoinError {}
+impl Default for JoinError {
+    fn default() -> Self {
+        Self(Box::new(std::io::Error::new(
+            ErrorKind::Other,
+            "aborted".to_string(),
+        )))
+    }
+}
 
 pub struct JoinHandle<T>
 where
     T: Send,
 {
-    #[cfg(target_arch = "wasm32")]
     handle: tokio::sync::oneshot::Receiver<T>,
-    #[cfg(not(target_arch = "wasm32"))]
-    handle: tokio::task::JoinHandle<T>,
 }
 
 #[cfg(target_arch = "wasm32")]
-pub mod wasm {
-
-    use std::{
-        future::Future,
-        pin::Pin,
-        task::{Context, Poll},
+macro_rules! spawn_impl {
+    ($fn:expr) => {
+        wasm_bindgen_futures::spawn_local($fn)
     };
-
-    use crate::runtime::{JoinError, JoinHandle};
-
-    pub fn spawn<F, T>(future: F) -> JoinHandle<T>
-    where
-        F: Future<Output = T> + 'static + Send,
-        T: Send + 'static,
-    {
-        let (sender, receiver) = tokio::sync::oneshot::channel();
-        wasm_bindgen_futures::spawn_local(async {
-            let result = future.await;
-            let result = sender.send(result);
-            if result.is_err() {
-                panic!("Failed to send result to receiver");
-            }
-        });
-
-        JoinHandle { handle: receiver }
-    }
-
-    impl<T> Future for JoinHandle<T>
-    where
-        T: Send,
-    {
-        type Output = Result<T, JoinError>;
-
-        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-            match Pin::new(&mut self.handle).poll(cx) {
-                Poll::Ready(Ok(val)) => Poll::Ready(Ok(val)),
-                Poll::Ready(Err(e)) => Poll::Ready(Err(JoinError { inner: e })),
-                Poll::Pending => Poll::Pending,
-            }
-        }
-    }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-pub mod native {
-
-    use crate::runtime::{JoinError, JoinHandle};
-
-    use std::{
-        future::Future,
-        pin::Pin,
-        task::{Context, Poll},
+macro_rules! spawn_impl {
+    ($fn:expr) => {
+        tokio::spawn($fn)
     };
+}
 
-    pub fn spawn<F, T>(future: F) -> JoinHandle<T>
-    where
-        F: Future<Output = T> + 'static + Send,
-        T: Send + 'static,
-    {
-        let handle = tokio::spawn(future);
-        JoinHandle { handle }
-    }
+pub fn spawn<F, T>(future: F) -> JoinHandle<T>
+where
+    F: Future<Output = T> + 'static + Send,
+    T: Send + 'static,
+{
+    let (sender, receiver) = tokio::sync::oneshot::channel();
+    spawn_impl!(async {
+        let result = future.await;
+        let _ = sender.send(result);
+    });
+    JoinHandle { handle: receiver }
+}
 
-    impl<T> Future for JoinHandle<T>
-    where
-        T: Send,
-    {
-        type Output = Result<T, JoinError>;
+impl<T> Future for JoinHandle<T>
+where
+    T: Send,
+{
+    type Output = Result<T, JoinError>;
 
-        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-            match Pin::new(&mut self.handle).poll(cx) {
-                Poll::Ready(Ok(val)) => Poll::Ready(Ok(val)),
-                Poll::Ready(Err(e)) => Poll::Ready(Err(JoinError { inner: e })),
-                Poll::Pending => Poll::Pending,
-            }
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match Pin::new(&mut self.handle).poll(cx) {
+            Poll::Ready(Ok(val)) => Poll::Ready(Ok(val)),
+            Poll::Ready(Err(_)) => Poll::Ready(Err(JoinError::default())),
+            Poll::Pending => Poll::Pending,
         }
     }
 }
