ac441a662b315235fa3fc27ab151282f01063b62
irvingouj @ Devolutions <jou@devolutions.net>
2024-12-04 07:42:40 -0500

    fix:remove unused memcpy function (#406)
    Co-authored-by: Eugene <inbox@null.page>

--- cryptovec/src/platform/wasm.rs
+++ cryptovec/src/platform/wasm.rs
@@ -13,13 +13,3 @@ pub fn memset(ptr: *mut u8, value: i32, size: usize) {
         std::ptr::write_bytes(ptr, byte_value, size);
     }
 }
-
-pub fn memcpy(dest: *mut u32, src: *const u8, size: usize) {
-    unsafe {
-        // Convert dest to *mut u8 for byte-wise copying
-        let dest_bytes = dest as *mut u8;
-
-        // Use std::ptr::copy_nonoverlapping to copy the data
-        std::ptr::copy_nonoverlapping(src, dest_bytes, size);
-    }
-}
