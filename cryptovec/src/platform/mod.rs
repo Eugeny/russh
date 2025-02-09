e574068f11a89c3250616bfb99eb8394d85a1126
Eugene <x@null.page>
2025-02-09 18:31:10 +0100

    fixed #460 - do not panic on mlock failures (#464)

--- cryptovec/src/platform/mod.rs
+++ cryptovec/src/platform/mod.rs
@@ -17,6 +17,45 @@ pub use wasm::{memset, mlock, munlock};
 #[cfg(windows)]
 pub use windows::{memset, mlock, munlock};
 
+#[cfg(not(target_arch = "wasm32"))]
+mod error {
+    use log::warn;
+    use std::error::Error;
+    use std::fmt::Display;
+    use std::sync::atomic::{AtomicBool, Ordering};
+
+    #[derive(Debug)]
+    pub struct MemoryLockError {
+        message: String,
+    }
+
+    impl MemoryLockError {
+        pub fn new(message: String) -> Self {
+            let warning_previously_shown = MLOCK_WARNING_SHOWN.swap(true, Ordering::Relaxed);
+            if !warning_previously_shown {
+                warn!("Security warning: OS has failed to lock/unlock memory for a cryptographic buffer: {}", message);
+                #[cfg(unix)]
+                warn!("You might need to increase the RLIMIT_MEMLOCK limit.");
+                warn!("This warning will only be shown once.");
+            }
+            Self { message }
+        }
+    }
+
+    static MLOCK_WARNING_SHOWN: AtomicBool = AtomicBool::new(false);
+
+    impl Display for MemoryLockError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "failed to lock/unlock memory: {}", self.message)
+        }
+    }
+
+    impl Error for MemoryLockError {}
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub use error::MemoryLockError;
+
 #[cfg(test)]
 mod tests {
     use super::*;
