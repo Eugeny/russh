7a64e632f0dcf2f5481e9d6521c25d79061e62b2
iHsin <root@itsusinn.eu.org>
2025-03-04 08:59:35 +0800

    better

--- cryptovec/src/platform/unix.rs
+++ cryptovec/src/platform/unix.rs
@@ -7,21 +7,21 @@ use super::MemoryLockError;
 /// Unlock memory on drop for Unix-based systems.
 pub fn munlock(ptr: *const u8, len: usize) -> Result<(), MemoryLockError> {
     unsafe {
+        Errno::clear();
         let ptr = NonNull::new_unchecked(ptr as *mut c_void);
         nix::sys::mman::munlock(ptr, len).map_err(|e| {
             MemoryLockError::new(format!("munlock: {} (0x{:x})", e.desc(), e as i32))
         })?;
-        Errno::clear();
     }
     Ok(())
 }
 
 pub fn mlock(ptr: *const u8, len: usize) -> Result<(), MemoryLockError> {
     unsafe {
+        Errno::clear();
         let ptr = NonNull::new_unchecked(ptr as *mut c_void);
         nix::sys::mman::mlock(ptr, len)
             .map_err(|e| MemoryLockError::new(format!("mlock: {} (0x{:x})", e.desc(), e as i32)))?;
-        Errno::clear();
     }
     Ok(())
 }
