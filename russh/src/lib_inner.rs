6dc491947e66231f9cfbc5f23ccd17d005eff052
Eugene <inbox@null.page>
2026-05-26 15:24:50 +0200

    expose channel number from ChannelId

--- russh/src/lib_inner.rs
+++ russh/src/lib_inner.rs
@@ -217,7 +217,9 @@ pub enum Error {
     #[error(transparent)]
     Elapsed(#[from] tokio::time::error::Elapsed),
 
-    #[error("Violation detected during strict key exchange, message {message_type} at seq no {sequence_number}")]
+    #[error(
+        "Violation detected during strict key exchange, message {message_type} at seq no {sequence_number}"
+    )]
     StrictKeyExchangeViolation {
         message_type: u8,
         sequence_number: usize,
@@ -425,6 +427,14 @@ impl ChannelOpenFailure {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd)]
 /// The identifier of a channel.
 pub struct ChannelId(u32);
+
+impl ChannelId {
+    // Not public to prevent construction of invalid
+    // ChannelIds by the library user
+    pub fn number(&self) -> u32 {
+        self.0
+    }
+}
 
 impl Decode for ChannelId {
     type Error = ssh_encoding::Error;
