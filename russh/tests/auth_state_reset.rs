9422a5d8f7e7b390acb72c907b4c59b7dc273235
Eugene <inbox@null.page>
2026-05-26 23:01:22 +0200

    fmt

--- russh/tests/auth_state_reset.rs
+++ russh/tests/auth_state_reset.rs
@@ -1,9 +1,8 @@
 use std::sync::Arc;
 use std::time::Duration;
 
-use russh::client;
 use russh::keys::PrivateKey;
-use russh::{MethodKind, MethodSet, server};
+use russh::{MethodKind, MethodSet, client, server};
 
 struct AcceptTestServerKey;
 
